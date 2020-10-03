4fccc31b627a35bbd9b857e9af4c591d2c9f94d0
Manish Pal <62168967+manish0pal@users.noreply.github.com>
2020-10-03 16:24:03 +0530

    Update practice_code.c

--- practice_code.c
+++ practice_code.c
@@ -149,3 +149,59 @@ int main() {
 
     return 0;
 }
+
+// Online C compiler to run C program online
+#include <stdio.h>
+int reverseOfArray(){
+   int a[5] = {1,2,3,4,5};
+   int temp;
+   for(int i =0;i<=5/2;i++){
+       temp = a[i];
+       a[i]= a[5-i-1];
+       a[5-i-1] = temp;
+   }
+   for(int i =0;i<=4;i++){
+       printf("%d\n",a[i]);
+   }
+    }
+int main() {
+    reverseOfArray();
+    for(int i = 1;i<=5;i++){
+            for(int j = 1;j<=5;j++){
+                printf("* ");
+            }
+            printf("\n");
+    }/*
+    * * * * * 
+    * * * * * 
+    * * * * * 
+    * * * * * 
+    * * * * * */
+
+    //Pattern one
+    for(int i = 1;i<=5;i++){
+            for(int j = 1;j<=i;j++){
+                printf("* ");
+            }
+            printf("\n");
+        }/*
+        * 
+        * * 
+        * * * 
+        * * * * 
+        * * * * * */
+    for(int i = 1;i<=5;i++){
+            for(int j = 5;j>=i;j--){
+                printf("* ");
+            }
+            printf("\n");
+    }/*
+    * * * * * 
+    * * * * 
+    * * * 
+    * * 
+    * 
+    */
+    
+    return 0;
+}
